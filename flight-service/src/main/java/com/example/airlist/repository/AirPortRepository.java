6a3b5d2d394f9e0c005bc88d1e4bf3ded93ceca8
PSI <parksj0199@naver.com>
2025-05-09 14:51:01 +0900

    20250509

--- flight-service/src/main/java/com/example/airlist/repository/AirPortRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.airlist.repository;
-
-import com.example.airlist.entity.Airport;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AirPortRepository extends JpaRepository<Airport, Long> {
-}
--- reservation-service/src/main/java/com/example/demo/reservationservice/dto/PaymentStatusResponse.java
+++
@@ -1,3 +0,0 @@
-package com.example.demo.reservationservice.dto;
-
-public record PaymentStatusResponse(String status) {}
